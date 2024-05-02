7647e7ffc0250d955ea5ad2e27851ca856f87756
roshan-42 <roshanacharya207@gmail.com>
2024-05-02 16:38:25 +0545

    cart finished

--- app/components/Ecommerce/Cart.tsx
+++ app/components/Ecommerce/Cart.tsx
@@ -1,32 +1,91 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Cart = () => {
-  let product_dataJSON = localStorage.getItem("products");
-  let product_data = product_dataJSON ? JSON.parse(product_dataJSON) : "";
-  console.log(product_data);
+  const [cartItems, setCartItems] = useState([]);
+  const storeProduct = (item: any) => {
+    let initialdataJSON = localStorage.getItem("products");
+    let initialData = initialdataJSON ? JSON.parse(initialdataJSON) : [];
+    console.log("initial data___________", initialData);
+    initialData.push(item);
 
-  let item_quantity = (id) => {
-    return product_data.filter((item) => item.id == id).length;
+    localStorage.setItem("products", JSON.stringify(initialData));
+    setCartItems(initialData);
   };
-  console.log("Item_quantity_________", item_quantity);
+
+  const decrement = (itemID) => {
+    const indexToremove = cartItems.findIndex((item) => item.id == itemID);
+    let updatedCart = [...cartItems];
+    updatedCart.splice(indexToremove, 1);
+    localStorage.setItem("products", JSON.stringify(updatedCart));
+    setCartItems(updatedCart);
+  };
+
+  useEffect(() => {
+    const cartDataJSON = localStorage.getItem("products");
+    const cartData = cartDataJSON ? JSON.parse(cartDataJSON) : [];
+    setCartItems(cartData);
+  }, []);
+
+  const itemQuantity = (itemID) => {
+    return cartItems.filter((item) => item.id == itemID).length;
+  };
+  console.log("total items======================", itemQuantity);
+
+  const itemOccurance = () => {
+    let sameItems = [];
+    cartItems.map((item) => {
+      const checkedSameID = sameItems.find((sItems) => sItems.id === item.id);
+      if (!checkedSameID) {
+        sameItems.push(item);
+      }
+    });
+    return sameItems;
+  };
+
+  const removeItem = (itemID) => {
+    const newCartItems = cartItems.filter((nitem) => nitem.id !== itemID);
+
+    setCartItems(newCartItems);
+    return newCartItems;
+  };
 
   return (
     <div className="bg-white h-screen flex items-center justify-center">
       <div className="w-[40%]">
         <p>Cart Items:</p>
         <div className=" shadow-md flex flex-wrap gap-10">
-          {product_data.map((item: any, index: any) => (
+          {itemOccurance().map((item, index) => (
             <div className="bg-white shadow-lg p-5 px-10" key={index}>
-              <div className="flex ">
+              <div className="flex">
                 <p>Product Name:</p>
                 <span>{item.name} </span>
-                <div>Quantity:{item_quantity(item.id)}</div>
               </div>
-
               <p>
                 Price:<span>{item.price} </span>
               </p>
+              <div className="flex gap-3">
+                <button
+                  onClick={() => decrement(item.id)}
+                  className="bg-black text-white p-1 rounded-md w-4"
+                >
+                  -
+                </button>
+                <div>Quantity: {itemQuantity(item.id)} </div>
+                <button
+                  onClick={() => storeProduct(item)}
+                  className="bg-black text-white p-1 rounded-md w-4"
+                >
+                  +
+                </button>
+              </div>
+              <button
+                onClick={() => removeItem(item.id)}
+                className="bg-red-400 px-5 rounded-lg mt-5"
+              >
+                {" "}
+                Remove
+              </button>
             </div>
           ))}
         </div>
