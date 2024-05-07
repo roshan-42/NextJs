39c6e3b024775fd8c09df46dc0e350d74bba6ace
roshan-42 <roshanacharya207@gmail.com>
2024-05-07 13:55:37 +0545

    todo done

---
+++ app/components/3. todoAPi/TodoApi.tsx
@@ -0,0 +1,163 @@
+"use client";
+import React, { useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const TodoApi = () => {
+  const [show, setShow] = useState(true);
+  const [title, setTitle] = useState("");
+  const [storeID, setStoreID] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const { data, error, mutate } = useSWR(
+    "http://127.0.0.1:8000/todo/",
+    fetcher
+  );
+  console.log("data..............", data);
+
+  //   =====================Post request==================================
+
+  const handleAdd = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description,
+      }),
+    };
+    const response = await fetch("http://127.0.0.1:8000/todo/", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        mutate();
+        setTitle("");
+        setDescription("");
+      })
+      .catch((error) => console.error(error));
+  };
+
+  //======================================Delete Request=============================================
+
+  const handleDelete = async (itemID) => {
+    const requestOptions = {
+      method: "DELETE",
+    };
+    const response = await fetch(
+      `http://127.0.0.1:8000/todo/${itemID}/`,
+      requestOptions
+    )
+      .then((response) => {
+        // response.json();
+        console.log(response);
+        if (response.status === 204) {
+          mutate();
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  //   =====================================Patch Request=================================
+
+  const handleUpdate = async () => {
+    const requestOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description,
+      }),
+    };
+    const response = await fetch(
+      `http://127.0.0.1:8000/todo/${storeID}/`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        mutate();
+        setTitle("");
+        setStoreID(0);
+        setDescription("");
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div className="text-black  h-screen flex flex-col gap-10 items-center justify-center">
+      <div className="flex flex-col gap-2">
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          className="p-3 rounded-xl"
+          type="text"
+          value={title}
+          placeholder="enter title here"
+        />
+        <input
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          className="p-3 rounded-xl"
+          value={description}
+          type="text"
+          placeholder="enter title description"
+        />
+        <button
+          onClick={handleAdd}
+          className={` ${
+            !show ? "hidden" : "block"
+          } bg-blue-400 px-4 rounded-md text-white`}
+        >
+          Add
+        </button>
+        <button
+          onClick={() => {
+            setShow(!show);
+            handleUpdate();
+          }}
+          className={` ${
+            !show ? "block" : "hidden"
+          }   bg-blue-400 px-4 rounded-md text-white`}
+        >
+          Update
+        </button>
+      </div>
+
+      <div>
+        {data?.map((item, index) => (
+          <div className="flex justify-between gap-5">
+            <div className="flex flex-col gap-3">
+              <p className="font-bold text-xl">{item.title}</p>
+              <p>{item.description}</p>
+            </div>
+
+            <button
+              onClick={() => handleDelete(item.id)}
+              className="bg-red-500 text-white px-3 border rounded-xl  "
+            >
+              Delete
+            </button>
+
+            <button
+              onClick={() => {
+                setShow(false);
+                setTitle(item.title);
+                setDescription(item.description);
+                setStoreID(item.id);
+              }}
+              className="bg-red-500 text-white px-3 border rounded-xl "
+            >
+              Update
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TodoApi;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Image from "next/image";
 import Home from "./components/Landing/Home";
 import Try from "./components/Landingrev/Try";
@@ -6,6 +7,7 @@ import Mainfilemodal from "./components/1. Latest/Mainfilemodal";
 import Mainfile from "./components/1. Latest/Mainfile";
 import Todo from "./components/2. ToDo practice/Todo";
 import Products from "./components/Ecommerce/Products";
+import TodoApi from "./components/3. todoAPi/TodoApi";
 
 export default function Page() {
   return (
@@ -16,6 +18,7 @@ export default function Page() {
       {/* <Mainfile />
       <Todo /> */}
       <Products />
+      <TodoApi />
     </div>
   );
 }
